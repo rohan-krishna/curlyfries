6e9c489b411c53182e3bbd348b0119542d6363c6
Rohan Krishna <=>
2017-04-11 20:23:04 +0530

    Immigration Login & Logout logics added

--- immigration/urls.py
+++ immigration/urls.py
@@ -8,4 +8,6 @@ app_name = 'immigration'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^login/$', views.login, name='login'),
+	url(r'^post_login/$', views.post_login, name='post_login'),
+	url(r'^logout/$', views.logout, name='logout'),
 ]
--- immigration/views.py
+++ immigration/views.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as djangoLogin
+from django.contrib.auth import logout as djangoLogout
 
 # Create your views here.
 def index(request):
@@ -19,3 +22,20 @@ def login(request):
 		return render(request, 'immigration/auth/login.html',{})
 	else:
 		return HttpResponseRedirect(reverse('immigration:index'))
+
+def post_login(request):
+	username = request.POST['username']
+	password = request.POST['password']
+
+	user = authenticate(username=username, password=password)
+
+	if user is not None:
+		djangoLogin(request, user)
+		return HttpResponseRedirect(reverse('immigration:index'))
+	else:
+		return HttpResponseRedirect(reverse('immigration:index'))
+
+
+def logout(request):
+	djangoLogout(request)
+	return HttpResponseRedirect(reverse('immigration:index'))
