8918c126e5514d38f8f998e9d662cf827d4b80f5
Rohan Krishna <Rohan.Krishna@csscorp.com>
2017-04-21 15:48:24 +0530

    Local Settings

--- curlyfries/settings.py
+++ curlyfries/settings.py
@@ -77,13 +77,23 @@ WSGI_APPLICATION = 'curlyfries.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'curlyfries',
+        'USER': 'postgres',
+        'PASSWORD': '30993',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
