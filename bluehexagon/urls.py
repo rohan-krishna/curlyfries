4d40ea331c0a4a4edd03b6c5ce84c424ce721d85
Rohan Krishna <Rohan.Krishna@csscorp.com>
2017-04-28 19:01:55 +0530

    Logout URL Added

--- bluehexagon/urls.py
+++ bluehexagon/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-app_name = 'bluhexagon'
+app_name = 'bluehexagon'
 
 urlpatterns = [
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	
 ]
--- centaur/urls.py
+++ centaur/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'centaur'
 
@@ -10,4 +11,5 @@ urlpatterns = [
 	url(r'^create/updatestory/$', views.updatestory, name='updatestory'),
 	url(r'^show/(?P<story_id>[0-9]+)/$', views.show, name='show'),
 	url(r'^delete/(?P<story_id>[0-9]+)/$', views.delete, name='delete'),
+	url(r'^logout/$', auth_views.logout, name='logout'),
 ]
